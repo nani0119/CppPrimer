7eec24e380a3b2cbb3a95317013cea291caf8844
nani <renjie.0501@163.com>
2019-09-17 18:25:56 +0800

    增加练习

--- PartII/Chapter10/s10-3/s10_3_4_bind_func.cc
+++ PartII/Chapter10/s10-3/s10_3_4_bind_func.cc
@@ -7,9 +7,27 @@
 
 using namespace std;
 
+ostream& print(ostream& os, const string& s, char c)
+{
+	os << s << c;
+	return os;
+}
+
+bool check_size(const string& str, string::size_type sz)
+{
+	return str.size() < sz;
+}
+
 int main(int argc, char *argv[])
 {
+	cout << boolalpha;
 	// auto newCallable = bind(callable, arg_list);
-		
+	auto check_size6 = bind(check_size, placeholders::_1, 6); // _n represents newCallable nth parater, others is fixed
+
+	cout << check_size6("hello") << endl;
+
+
+	auto f = bind(print, ref(cout), placeholders::_1, '\n');
+	f("hello");
 	return 0;
 }
