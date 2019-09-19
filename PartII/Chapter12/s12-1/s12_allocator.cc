82d277c690a019b60051b0e08face31c178eb390
nani <renjie.0501@163.com>
2019-09-19 21:09:41 +0800

    增加练习

--- PartII/Chapter12/s12-1/s12_allocator.cc
+++ PartII/Chapter12/s12-1/s12_allocator.cc
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+	const int n = 5;
+	allocator<string> alloc;       // object that can allocate strings
+	auto const p = alloc.allocate(n);    // allocate n unconstructed strings
+
+	auto q = p;   // q will point to one past the last constructed element
+	alloc.construct(q++);            // *q is the empty string
+	alloc.construct(q++, 10, 'c');   // *q is cccccccccc
+	alloc.construct(q++, "hi");      // *q is hi!
+
+	cout << *p << endl;
+	cout << *(p+1) << endl;
+	cout << *(p+2) << endl;
+	// cout << *q << endl;  // disaster: q points to unconstructed memory!
 	
+	while (q != p)
+	{
+		alloc.destroy(--q);  // free the strings we actually allocated
+	}
+
+	alloc.deallocate(p, n);  
 	return 0;
 }
